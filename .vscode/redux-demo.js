7f4603661d7b82fc36185c3f9d9e27a4b7c507ff
shriya <komishettyshriya@gmail.com>
2023-01-29 22:36:54 +0530

    reduxproject

--- .vscode/redux-demo.js
+++ .vscode/redux-demo.js
@@ -1,1 +1,18 @@
+
 const redux = require('redux'); //importing
+const redux_mock_store = require("redux-mock-store")
+const configureStore = redux_mock_store.configureStore
+
+const counterReducer = (state = {counter: 0},action) => {
+    return {
+        counter: state.counter + 1 
+    };
+};
+const store = redux.configureStore(counterReducer); 
+
+
+const counterSubscriber = () => {
+ const latestState = store.getState();
+ console.log(latestState);
+}; //to get latest state
+store.subscribe(counterSubscriber);
--- README.md
+++ README.md
@@ -8,5 +8,13 @@ npm install
 npm install redux 
 In the file - (redux- demo.js)
 1. Import it
-2. 
+2. Create a store (storing all data and manages with reducer function)
+3. Create Reducer function before the store , inputs are old state + dispatched state
+ We have both store and reducer , they work together to get state
+4. We add another reducer function for change of state called subsriber function 
+We have reducer and subscriber
+
+
+
+
 
