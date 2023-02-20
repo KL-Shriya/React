8db5e4f4c30858c5e65352b5eae885a331f6f7d9
shriya <komishettyshriya@gmail.com>
2023-02-21 00:28:05 +0530

    react

--- redux/src/src/store/index.js
+++ redux/src/src/store/index.js
@@ -25,6 +25,25 @@ const counterSlice = createSlice({
    } //automatically called
 });
 
+const initialAuthState = {
+  isAuthenticated: false
+};
+
+const authSlice = createSlice({
+  name: 'authentication',
+  initialState: initialAuthState ,
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true ;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
+    }
+  }
+});
+
+
+
 // const counterReducer = (state = { counter: 0 }, action) => {
 //   if (action.type === 'increment') {
 //     return {
@@ -64,9 +83,12 @@ const counterSlice = createSlice({
 
 
 const store = ConfigureStore({
-  reducer: counterSlice.reducer
+  reducer: {counter: counterSlice.reducer , auth: authSlice.reducer },
 });
 
 
+
 export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
+
 export default store;
